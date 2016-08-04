52884710de210a2abb9919a8fff700b2ffbf2efc
jalavosus <alavosus.james@gmail.com>
2016-08-04 13:28:41 -0400

    Hotkeys work

--- AutoBotCSharp/App.xaml.cs
+++ AutoBotCSharp/App.xaml.cs
@@ -14,15 +14,17 @@ namespace AutoBotCSharp
     /// </summary>
     public partial class App : Application
     {
+        private static Random randy = new Random();
         /*
          * RollTheClip is a method that's part of the application logic, not the Form logic. Therefore, it should be in the App class.
          */
         public static bool RollTheClip(string Clip)
         {
+            Console.WriteLine("CLIP");
             try
             {
                 Mp3FileReader Reader = new Mp3FileReader(Clip);
-                var mp3 = new NAudio.Wave.WaveOut();
+                var mp3 = new WaveOut();
                 mp3.Init(Reader);
                 mp3.Play();
                 return true;
@@ -33,7 +35,28 @@ namespace AutoBotCSharp
                 Console.WriteLine(ex.InnerException);
                 return false;
             }
-
+        }
+        public static void playOkClip()
+        {
+            string[] okClips = new string[] {
+                @"C:\Soundboard\Cheryl\REACTIONS\OK.mp3",
+                @"C:\Soundboard\Cheryl\REACTIONS\OK2.mp3",
+                @"C:\Soundboard\Cheryl\REACTIONS\okGreat.mp3",
+            };
+            int index = randy.Next(okClips.Length);
+            string clip = okClips[index];
+            RollTheClip(clip);
+        }
+        public static void playHumanism()
+        {
+            string[] humanismClips = new string[]
+            {
+                @"C:\Soundboard\Cheryl\REACTIONS\Excellent2.mp3",
+                @"C:\Soundboard\Cheryl\REACTIONS\Great 2.mp3",
+            };
+            int index = randy.Next(humanismClips.Length);
+            string clip = humanismClips[index];
+            RollTheClip(clip);
         }
     }
 }
--- AutoBotCSharp/MainWindow.xaml.cs
+++ AutoBotCSharp/MainWindow.xaml.cs
@@ -13,7 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using NAudio.Wave;
-
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
 
 namespace AutoBotCSharp
 {
@@ -22,14 +23,14 @@ namespace AutoBotCSharp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Random randy;
+
+        
         public MainWindow()
         {
+            randy = new Random();
             InitializeComponent();
         }
-
-
-
-
 
         private void btnHello_Click(object sender, RoutedEventArgs e)
         {
@@ -41,25 +42,90 @@ namespace AutoBotCSharp
         {
 
         }
-
-        private void btnLaugh_Copy_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
         private void tabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
-
-        private void btnIntro_Copy4_Click(object sender, RoutedEventArgs e)
         {
 
         }
 
         private void btnOk_Click(object sender, RoutedEventArgs e)
         {
+            App.playOkClip();
+        }
 
+        private void btnIntro_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        /*
+         * Here be dragons.  
+         */ 
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(
+            IntPtr hWnd,
+            int id,
+            int fsModifiers,
+            int vk);
+        // RegisterHotKey takes a window handle (normally this.Handle), an ID (whatever we want), a modifer key -- fsModifiers --  (alt, ctrl, etc) and an int-ified letter -- vk -- (ie. (int)'A', (int)Keys.F1s)
+        // We should start ids at 0.
+        /* modifier key values: 
+         * ALT: 0x1
+         * CTRL: 0x2
+         * Shift: 0x4
+         * Windows Key: 0x8
+         */
+        /* ID numbers and their assignments (add to this for reference)
+         * 0 : 'OK' and related
+         * 1 : Humanism button
+         */
+        [DllImport("user32.dll")]
+        public static extern bool UnregisterHotkey(
+            IntPtr hWnd,
+            int id);
+        private HwndSource _source;
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+            var helper = new WindowInteropHelper(this);
+            _source = HwndSource.FromHwnd(helper.Handle);
+            _source.AddHook(HwndHook);
+            RegisterHotkeys();
+        }
+        protected override void OnClosed(EventArgs e)
+        {
+            _source.RemoveHook(HwndHook);
+            _source = null;
+            UnregisterHotkeys();
+            base.OnClosed(e);
+        }
+        private void RegisterHotkeys()
+        {
+            var helper = new WindowInteropHelper(this);
+            RegisterHotKey(helper.Handle, 0, 0, 0x70); // F1
+        }
+        private void UnregisterHotkeys()
+        {
+            var helper = new WindowInteropHelper(this);
+            UnregisterHotkey(helper.Handle, 0);
+        }
+        private IntPtr HwndHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            const int WM_HOTKEY = 0x0312;
+            switch (msg)
+            {
+                case WM_HOTKEY:
+                    switch (wParam.ToInt32())
+                    {
+                        case 0:
+                            App.playOkClip();
+                            break;
+                    }
+                    break;
+            }
+
+
+            return IntPtr.Zero;
         }
     }
 }
