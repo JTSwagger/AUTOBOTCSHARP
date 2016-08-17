56b0e478d04544ea274ee8e18c5a34d921dbe393
jalavosus <alavosus.james@gmail.com>
2016-08-17 16:12:35 -0400

    theoretically it never gets disposed of now

--- AutoBotCSharp/App.xaml.cs
+++ AutoBotCSharp/App.xaml.cs
@@ -63,6 +63,7 @@ namespace AutoBotCSharp
         }
         public static void onMicrophoneStatusHandler(object sender, MicrophoneEventArgs e)
         {
+            GC.KeepAlive(longDictationClient);
             Agent temp = getAgent();
             Console.WriteLine(e.Recording);
             temp.isListening = e.Recording;
@@ -74,6 +75,7 @@ namespace AutoBotCSharp
 
         public static void onPartialResponseReceivedHandler(object sender, PartialSpeechResponseEventArgs e)
         {
+            GC.KeepAlive(longDictationClient);
             Console.WriteLine(getAgent().Question);
             string response = e.PartialResult;
             Current.Dispatcher.Invoke((async () =>
@@ -94,7 +96,7 @@ namespace AutoBotCSharp
 
         public static void onResponseReceivedHandler(object sender, SpeechResponseEventArgs e)
         {
-            
+            GC.KeepAlive(longDictationClient);
             Console.WriteLine(e.PhraseResponse.RecognitionStatus);
             if (e.PhraseResponse.RecognitionStatus == ((RecognitionStatus)611) || e.PhraseResponse.RecognitionStatus.ToString() == "611")
             {
@@ -104,13 +106,6 @@ namespace AutoBotCSharp
             }
             if (e.PhraseResponse.RecognitionStatus == RecognitionStatus.DictationEndSilenceTimeout)
             {
-                //Console.WriteLine("Burning through another api call! Yay!");
-                //longDictationClient.EndMicAndRecognition();
-                //string apiKey1 = "ce43e8a4d7a844b1be7950b260d6b8bd";
-                //string apiKey2 = "0d2797650c8648d18474399744512f17";
-                //longDictationClient = SpeechRecognitionServiceFactory.CreateMicrophoneClient(SpeechRecognitionMode.LongDictation, "en-US", apiKey1, apiKey2);
-                //longDictationClient.OnPartialResponseReceived += App.onPartialResponseReceivedHandler;
-                //longDictationClient.OnResponseReceived += App.onResponseReceivedHandler;
                 longDictationClient.EndMicAndRecognition();
                 longDictationClient.StartMicAndRecognition();
             }
@@ -146,6 +141,7 @@ namespace AutoBotCSharp
 
         public static void doBackgroundQuestionSwitchingStuff()
         {
+            GC.KeepAlive(longDictationClient);
             Agent ag = getAgent();
             // call position advancement
             switch (ag.Question)
